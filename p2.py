2cab791a282ac95868d2f2056e1fd047f81065c5
Makur123 <makurdhuor2019@gmail.com>
2026-07-31 22:20:47 +0530

    The second branch have added the addition feature of the calculator

--- p2.py
+++ p2.py
@@ -3,3 +3,10 @@ if a > 0:
     print("The number is positive.")    
 else:
     print("The number is not positive.")
+b = int(input("Enter another number: "))
+if b > 0:   
+    print("The number is positive.")            
+else:
+    print("The number is not positive.")
+    
+print("The sum is: " + str(a + b))
